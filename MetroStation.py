5be43de6793767e70c7709ef93640756a32ccc5e
BTECHMEMORIES <127437693+BTECHMEMORIES@users.noreply.github.com>
2025-09-18 22:33:20 +0530

    Update MetroStation.py

--- MetroStation.py
+++ MetroStation.py
@@ -2,5 +2,6 @@ class MetroStation:
     def __init__(self, name):
         self.name = name
         self.visited = False
-        self.connections = {}  # Equivalent to Map<MetroStation, Integer>
-        self.platforms = {}    # Equivalent to HashMap<MetroStation, Integer>
+        self.connections = {}  
+        self.platforms = {}    
+
