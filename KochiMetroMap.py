e6a80f564851af6ba3319eb9519fa4333048ef17
BTECHMEMORIES <127437693+BTECHMEMORIES@users.noreply.github.com>
2025-09-18 22:20:32 +0530

    Update and rename KochiMetroMap.java to KochiMetroMap.py

--- KochiMetroMap.py
+++ KochiMetroMap.py
@@ -5,11 +5,9 @@ import dsa.project.functionality.MetroMap;
 public class KochiMetroMap extends MetroMap {
 
     public KochiMetroMap() {
-    	
-    	city = "Kochi";
-    	
-        // Blue Line
-    	CreatePath("nullAluva", "Aluva", "blue", 0);
+        city = "Kochi";
+
+        CreatePath("Terminal_Aluva", "Aluva", "blue", 0);
         CreatePath("Aluva", "Pulinchodu", "blue", 1);
         CreatePath("Pulinchodu", "Companypady", "blue", 2);
         CreatePath("Companypady", "Ambattukavu", "blue", 3);
@@ -32,27 +30,25 @@ public class KochiMetroMap extends MetroMap {
         CreatePath("Vyttila", "Thaikoodam", "blue", 1);
         CreatePath("Thaikoodam", "Petta", "blue", 2);
         CreatePath("Petta", "Kakkanad", "blue", 1);
-        CreatePath("Kakkanad", "JLN Stadium", "blue", 1);
-        CreatePath("JLN Stadium", "Infopark", "blue", 1);
+        CreatePath("Kakkanad", "Infopark", "blue", 1);
         CreatePath("Infopark", "Technocity", "blue", 1);
         CreatePath("Technocity", "Kinfra", "blue", 1);
         CreatePath("Kinfra", "Cusat", "blue", 2);
-        CreatePath("Cusat", "Pathadipalam", "blue", 3); 
-        CreatePath("Pathadipalam", "nullPathadipalam", "blue", 0);// Additional station
+        CreatePath("Cusat", "Terminal_Cusat", "blue", 0);
 
         // Purple Line
-        CreatePath("nullJawaharlal Nehru Stadium", "Jawaharlal Nehru Stadium", "purple", 0);
+        CreatePath("Terminal_Jawaharlal Nehru Stadium", "Jawaharlal Nehru Stadium", "purple", 0);
         CreatePath("Jawaharlal Nehru Stadium", "Kaloor", "purple", 3);
         CreatePath("Kaloor", "Kadavanthra", "purple", 2);
         CreatePath("Kadavanthra", "Ernakulam South", "purple", 3);
         CreatePath("Ernakulam South", "Kochi Marina", "purple", 2);
         CreatePath("Kochi Marina", "Thevara", "purple", 3);
         CreatePath("Thevara", "Kundannoor", "purple", 1);
-        CreatePath("Kundannoor", "Pettah", "purple", 2);
-        CreatePath("Pettah", "Kacheripady", "purple", 1);
-        CreatePath("Kacheripady", "Changampuzha Park", "purple", 2); // Additional station
+        CreatePath("Kundannoor", "Petta", "purple", 2); // Unified spelling
+        CreatePath("Petta", "Kacheripady", "purple", 1);
+        CreatePath("Kacheripady", "Changampuzha Park", "purple", 2);
         CreatePath("Changampuzha Park", "Palarivattom", "purple", 3);
-        CreatePath("Palarivattom", "nullPalarivattom", "purple", 0);// Additional station
+        CreatePath("Palarivattom", "Terminal_Palarivattom", "purple", 0);
 
         setPlatforms();
     }
